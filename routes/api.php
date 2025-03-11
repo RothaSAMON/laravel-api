c69e749d71dd0b4a25ceadb38b530d78e7e021e8
RothaSAMON <samonrotha@gmail.com>
2025-03-11 11:06:03 +0700

    Tranforming database data into JSON

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// app/v1
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function() {
+    Route::apiResource('customers', CustomerController::class);
+    Route::apiResource('invoices', InvoiceController::class);
+});
